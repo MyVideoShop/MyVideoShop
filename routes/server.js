265a5b52bff9d3cc5a3f82fcd48230878a14c312
MyVideoShop <myvideoshop24.7@gmail.com>
2025-05-12 14:52:21 +0100

    Update server.js

--- routes/server.js
+++ routes/server.js
@@ -1,48 +1,8 @@
-const express = require("express");
-const session = require("express-session");
-const path = require("path");
-const http = require("http");
-const socketIO = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-
-let totalVisitors = 0;
-let onlineUsers = 0;
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
-  secret: "geheim",
-  resave: false,
-  saveUninitialized: true,
-}));
-
-// Besucher nur zählen, wenn von Startseite "/"
-app.get("/", (req, res) => {
-  if (!req.session.hasVisited) {
-    totalVisitors++;
-    req.session.hasVisited = true;
-  }
-  res.render("index", { totalVisitors });
+app.get('/admin', (req, res) => {
+  res.render('admin');
 });
 
-// Beispiel-Routen
-app.get("/admin", (req, res) => res.send("Admin-Bereich"));
-app.get("/creator", (req, res) => res.send("Creator-Bereich"));
-
-io.on("connection", (socket) => {
-  onlineUsers++;
-  io.emit("updateOnlineUsers", onlineUsers);
-
-  socket.on("disconnect", () => {
-    onlineUsers--;
-    io.emit("updateOnlineUsers", onlineUsers);
-  });
+app.get('/creator/:name', (req, res) => {
+  const name = req.params.name;
+  res.render('creator', { name });
 });
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
