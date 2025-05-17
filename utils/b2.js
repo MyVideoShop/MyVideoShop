27e98d825506a63282cf77faf5e0676b039043fb
MyVideoShop <myvideoshop24.7@gmail.com>
2025-05-17 12:04:42 +0100

    Create b2.js

---
+++ utils/b2.js
@@ -0,0 +1,23 @@
+// utils/b2.js
+const B2 = require('backblaze-b2');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const b2 = new B2({
+  applicationKeyId: process.env.B2_KEY_ID,
+  applicationKey: process.env.B2_APP_KEY
+});
+
+// Authentifiziere dich sofort beim Laden der Datei
+async function authorizeB2() {
+  try {
+    await b2.authorize();
+    console.log('✅ Mit Backblaze B2 verbunden.');
+  } catch (error) {
+    console.error('❌ Fehler beim Verbinden mit Backblaze B2:', error.message);
+  }
+}
+
+authorizeB2();
+
+module.exports = b2;
