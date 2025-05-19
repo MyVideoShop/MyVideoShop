050bd97abfd3ba6412a1606b5a502ac0151a6bbe
MyVideoShop <myvideoshop24.7@gmail.com>
2025-05-19 10:44:37 +0100

    Update b2.js

--- b2.js
+++ b2.js
@@ -5,7 +5,7 @@ const B2 = require('backblaze-b2');
 
 const b2 = new B2({
     applicationKeyId: process.env.B2_KEY_ID,
-    applicationKey: process.env.B2_APP_KEY
+    applicationKey: process.env.B2_APPLICATION_KEY
 });
 
 let authorized = false;
