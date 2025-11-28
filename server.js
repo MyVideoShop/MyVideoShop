bc54f9d3d0b1dc91aefc78da5927097e41bed25d
MyVideoShop <myvideoshop24.7@gmail.com>
2025-11-28 17:06:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Startseite
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Enhancer Script bereitstellen
-app.get('/enhancer.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'enhancer.js'));
-});
-
-// Start Server
 app.listen(PORT, () => {
-  console.log(`Streaming Hub läuft auf Port ${PORT}`);
+  console.log('Streaming Hub läuft!');
 });
